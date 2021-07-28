ded08715f11407719dfbc175e8b3f02944d62775
teddy-duncan <77077101+teddy-duncan@users.noreply.github.com>
2021-07-28 16:25:31 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+Webcam.set({
+    width:350,
+    height: 300,
+    image_format: 'png',
+    png_quailty: 90
+});
+camera=document.getElementById("camera");
+Webcam.attach('#camera');
+function take_snapshot() {
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML='<img id="capture_image" src="'+data_uri+'">';
+
+    });
+}
+console.log('ml5 version:',ml5.version);
+classifier=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/DyS85E2lZ/',modelLoaded);
+function modelLoaded(){
+    console.log('model loaded');
+}
+function check(){
+    img=document.getElementById('capture_image');
+    classifier.classify(img,gotResult);
+}
+function gotResult(error,results){
+    if(error){
+        console.error(error);
+    }
+    else{
+        console.log(results);
+        document.getElementById("result_object_name").innerHTML=results[0].label;
+        document.getElementById("result_object_accuracy").innerHTML=results[0].confidence.toFixed(3);
+    }
+}
